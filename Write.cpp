28512953029da3256ea39ad060897c7bf2d4d328
Logan Sritharan <ls718@ic.ac.uk>
2022-03-16 19:57:56 +0000

    namespace cleanup

--- Write.cpp
+++ Write.cpp
@@ -2,20 +2,18 @@
 
 #include <fstream>
 #include <iomanip>
-using namespace std;
 
 // Method to export data
 void ReactionDiffusion::Write() {
-    ofstream vOut("data.txt", ios::out | ios::trunc);
+    std::ofstream vOut("data.txt", std::ios::out | std::ios::trunc);
     vOut.precision(5);
 
     for (int j = 0; j < Ny; ++j) {
         for (int i = 0; i < Nx; ++i) {
-            vOut << i * 1.0 / double(Nx - 1) << " " << j * 1.0 / double(Ny - 1) << " " << u[Nx*j + i] << " " << v[Nx*j + i] << endl;
+            vOut << i * 1.0 / double(Nx - 1) << " " << j * 1.0 / double(Ny - 1) << " " << u[Nx*j + i] << " " << v[Nx*j + i] << std::endl;
         }
-        vOut << endl;
+        vOut << std::endl;
     }
 
     vOut.close();
-
 }
