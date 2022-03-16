7d45e15667aafc60fbde85ea8a93556a29418ec7
Logan Sritharan <ls718@ic.ac.uk>
2022-03-16 14:36:28 +0000

    Initial commit

---
+++ SetParameters.cpp
@@ -0,0 +1,20 @@
+#include "ReactionDiffusion.h";
+
+// Method to set parameters
+void ReactionDiffusion::SetParameters(double dt, double T, int Nx, int Ny, double a, double b, double mu1, double mu2, double eps) {
+    this->dt = dt;
+    this->T = T;
+
+    Nt = (int) T / dt;
+    
+    this->Nx = Nx;
+    this->Ny = Ny;
+    this->a = a;
+    this->b = b;
+    this->mu1 = mu1;
+    this->mu2 = mu2;
+    this->eps = eps;
+
+    lambda1 = mu1*dt/(h*h);
+    lambda2 = mu2*dt/(h*h);
+}
