c339a01dfa54f46525631e7694894f38723287ab
Logan Sritharan <ls718@ic.ac.uk>
2022-03-16 22:18:28 +0000

    moved mpi to main.cpp

--- ProcessArgs.h
+++ ProcessArgs.h
@@ -1,4 +1,4 @@
-#include <mpi.h>
+#include <iostream>
 #include <boost/program_options.hpp>
 
 int ProcessArgs(int&, int&, char**, double&, double&, int&, int&, double&, double&, double&, double&, double&);
