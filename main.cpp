ec29309027f736a45dcfaacf0d1100385f5b57e3
Logan Sritharan <ls718@ic.ac.uk>
2022-03-16 14:23:44 +0000

    Create class

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+// Class for computing Reaction Diffusion PDE (variant of Barkley model)
+class ReactionDiffusion {
+    private:
+        // Attributes
+        double dt, T;
+        int Nt;
+
+        double h = 1.0;
+        int Nx, Ny;
+
+        double a, b, mu1, mu2, eps;
+        double lambda1, lambda2;
+
+        double* u, v;
+        double* A, B;
+
+        // Methods
+        void SetParameters(double, double, int, int, double, double, double, double, double);
+        void SetInitialConditions();
+        void TimeIntegrate();
+};
