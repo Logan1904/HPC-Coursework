ff3a6945e8272a3dcb12422a161b1fd1a3d1360e
Logan Sritharan <ls718@ic.ac.uk>
2022-03-19 00:03:57 +0000

    OpenMP parallel for loop

--- TimeIntegrate.cpp
+++ TimeIntegrate.cpp
@@ -1,14 +1,41 @@
 #include "ReactionDiffusion.h"
 
+#include <iostream>
+#include <cblas.h>
+
 // Method to initialise A and B matrix
 void ReactionDiffusion::TimeIntegrate() {
-    double* utmp = new double[Nx*Ny]();
     int length = Nx*Ny;
-    for (int row = 0; row < length; ++row) {
-        utmp[row] += dt*()
-        for (int col = 0; col < length; ++col) {
-            utmp[row] += A[row*length + col] * u[row];
-        }
+    int t = 0;
+    while (t<Nt) {
+        double* utmp = new double[Nx*Ny]();
+        double* vtmp = new double[Nx*Ny]();
+
+        #pragma omp parallel for
+        
+            for (int row = 0; row < length; ++row) {
+                utmp[row] += dt*(eps*u[row]*(1.0-u[row])*(u[row] - (1/a)*(v[row]+b)));
+                vtmp[row] += dt*(u[row]*u[row]*u[row] - v[row]);
+            }
+        
+        
+        #pragma omp parallel for collapse(2) 
+        
+            for (int row = 0; row < length; ++row) {
+                for (int col = 0; col < length; ++col) {
+                    utmp[row] += A[row*length + col] * u[col];
+                    vtmp[row] += B[row*length + col] * v[col];
+                }
+            }
+        
+
+        cblas_dcopy(length, utmp, 1, u, 1);
+        cblas_dcopy(length, vtmp, 1, v, 1);
+
+        ++t;
+
+        delete[] utmp;
+        delete[] vtmp;
     }
 
 }
