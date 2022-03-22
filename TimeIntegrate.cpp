90e5b822af4d3a5bbf1633431b0de769554ec308
Logan Sritharan <ls718@ic.ac.uk>
2022-03-22 18:58:05 +0000

    New structure

--- TimeIntegrate.cpp
+++ TimeIntegrate.cpp
@@ -8,44 +8,40 @@
 
 // Method to initialise A and B matrix
 void ReactionDiffusion::TimeIntegrate() {
-    int t = 0;
-    while (t < Nt) {
+    for (int t = 0; t < Nt; ++t) {
         double* utmp = new double[Nx*Ny];
         double* vtmp = new double[Nx*Ny];
+        double* utmp2 = new double[Nx*Ny];
+        double* vtmp2 = new double[Nx*Ny];
         
         #pragma omp parallel
         {
-            #pragma omp single
+            #pragma omp sections nowait
             {   
-                #pragma omp task
+                #pragma omp section
                 cblas_dsbmv(CblasColMajor, CblasUpper, Nx*Ny, Nx, 1.0, A, Nx+1, u, 1, 0.0, utmp, 1); // Compute A*u[n]
 
-                #pragma omp task
+                #pragma omp section
                 cblas_dsbmv(CblasColMajor, CblasUpper, Nx*Ny, Nx, 1.0, B, Nx+1, v, 1, 0.0, vtmp, 1); // Compute B*v[n]
             }
 
             // Compute f1(u[n],v[n]) and f2(u[n],v[n])
-            #pragma omp for
+            #pragma omp for schedule(static)
                 for (int i = 0; i < Nx*Ny; ++i) {
-                    utmp[i] += dt*(eps*u[i]*(1-u[i])*(u[i] - (1/a)*(v[i]+b)));
-                    vtmp[i] += dt*(u[i]*u[i]*u[i] - v[i]);
+                    utmp2[i] = dt*(eps*u[i]*(1-u[i])*(u[i] - (1/a)*(v[i]+b)));
+                    vtmp2[i] = dt*(u[i]*u[i]*u[i] - v[i]);
                 }
-        
-            // Update u and v
-            #pragma omp single
-            {   
-                #pragma omp task
-                cblas_dcopy(Nx*Ny, utmp, 1, u, 1);
 
-                #pragma omp task
-                cblas_dcopy(Nx*Ny, vtmp, 1, v, 1);
-
-                #pragma omp task
-                ++t;
-            }
+            #pragma omp for nowait schedule(static)
+                for (int i = 0; i < Nx*Ny; ++i) {
+                    u[i] = utmp[i] + utmp2[i];
+                    v[i] = vtmp[i] + vtmp2[i];
+                }
         }
 
         delete[] utmp;
         delete[] vtmp;
+        delete[] utmp2;
+        delete[] vtmp2;
     }
 }
